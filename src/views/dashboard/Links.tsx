c38487979e686ee4d890d1939efbad26ebbc0441
Gabriel <gabriel.smith01@gmail.com>
2020-09-14 10:04:29 -0400

    updated links

--- src/views/dashboard/Links.tsx
+++ src/views/dashboard/Links.tsx
@@ -12,23 +12,28 @@ class Links extends React.PureComponent {
         <a href="https://mail.google.com">gmail</a>
         <a href="http://feedly.com">feedly</a>
         <a href="https://github.com">github</a>
-        <a href="https://calendar.google.com/calendar/render#main_7">calendar</a>
+        <a href="https://calendar.google.com/calendar/render#main_7">
+          calendar
+        </a>
         <a href="https://docs.google.com">docs</a>
         <a href="https://sheets.google.com">sheets</a>
         <b>Alchemy</b>
         <a href="https://github.com/pgalchemy">github - alchemy</a>
-        <a href="https://github.com/orgs/pgalchemy/projects/3">osa-project</a>
+        <a href="https://github.com/orgs/procter-gamble/teams/pgssa">pgssa</a>
+        <a href="https://github.com/orgs/procter-gamble/teams/ecaas">eCaaS</a>
         <ul>
-          <li><a href="https://github.com/pgalchemy/osa-visage-api">osa-visage-api</a></li>
-          <li><a href="https://github.com/pgalchemy/visage-service">visage-service</a></li>
-          <li><a href="https://github.com/pgalchemy/osa-core-api">osa-core-api</a></li>
-          <li><a href="https://github.com/pgalchemy/osa-skinadvisor-web">osa-skinadvisor-web</a></li>
-          <li><a href="https://github.com/pgalchemy/osa-web">osa-web</a></li>
-          <li><a href="https://github.com/pgalchemy/osa-retinol">osa-retinol</a></li>
-          <li><a href="https://github.com/pgalchemy/osa-retinol-web">osa-retinol-web</a></li>
+          <li>
+            <a href="https://github.com/procter-gamble/ecaas-fraud-syndication">
+              fraud-syndication
+            </a>
+          </li>
         </ul>
-        <a href="https://calendar.google.com/calendar/b/1/render#main_7">calendar - alchemy</a>
-        <a href="https://drive.google.com/drive/u/1/folders/0ALGRTPywK1LjUk9PVA">drive</a>
+        <a href="https://calendar.google.com/calendar/b/1/render#main_7">
+          calendar - alchemy
+        </a>
+        <a href="https://drive.google.com/drive/u/1/folders/0ALGRTPywK1LjUk9PVA">
+          drive
+        </a>
         <b>Dev</b>
         <a href="http://localhost:3000">localhost:3000</a>
         <a href="http://localhost:4200">localhost:4200</a>
